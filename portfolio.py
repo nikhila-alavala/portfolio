bc3abec34f7ca68fc594b2ec6d2e7ac1f86abac8
Nikhila Alavala <nalavala6@gatech.edu>
2025-08-22 09:44:06 -0400

    Trying to add PDF Preview with main ACM link

--- portfolio.py
+++ portfolio.py
@@ -61,26 +61,18 @@ def experience(data):
 
 st.header("Published Paper")
 
-# pdf_file = "Paper.pdf"
+with st.expander("Beyond Buzzwords: Making Sustainability a Pillar of the Computing Curriculum"):
+    # st.markdown(pdf_display, unsafe_allow_html=True)
 
-# with open(pdf_file, "rb") as f:
-#     base64_pdf = base64.b64encode(f.read()).decode("utf-8")
-
-# pdf_display = f"""
-#     <iframe src="data:application/pdf;base64,{base64_pdf}" 
-#     width="100%" height="800" type="application/pdf"></iframe>
-# """
-# with st.expander("Beyond Buzzwords: Making Sustainability a Pillar of the Computing Curriculum"):
-#     # st.markdown(pdf_display, unsafe_allow_html=True)
-
-#     pdf_url = "https://dl.acm.org/doi/pdf/10.1145/3724363.3729034"
+    pdf_url = "https://dl.acm.org/doi/10.1145/3724363.3729034"
     
-#     st.components.v1.html(
-#         f'<iframe src="{pdf_url}" width="100%" height="800"></iframe>',
-#         height=800
-#     )
-with st.expander("Beyond Buzzwords"):
-    st.download_button("Download PDF", data=open("Paper.pdf", "rb"), file_name="my_paper.pdf")
+    st.components.v1.html(
+        f'<iframe src="{pdf_url}" width="100%" height="800"></iframe>',
+        height=800
+    )
+
+# with st.expander("Beyond Buzzwords"):
+#     st.download_button("Download PDF", data=open("Paper.pdf", "rb"), file_name="Beyond Buzzwords: Making Sustainability a Pillar of the Computing Curriculum.pdf")
 
 st.header("Projects")
 project(info.projects_data)
